b99b1753791f3289ec4719757077177ddffb4a32
Tony Bark <noreply@tonybark.com>
2024-02-20 17:03:14 -0500

    Added more boids - Removed await keyword

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Flocks of Boids
+# Flock of Boids
 
 A simple artifical life simulation written in GDScript, and based on [this tutorial](https://youtu.be/oFnIlNW_p10?si=ge5FNIhLIuxY6Z0J).
 
--- scripts/boid.gd
+++ scripts/boid.gd
@@ -13,7 +13,7 @@ func _ready() -> void:
 	randomize()
 	
 func _physics_process(delta: float) -> void:
-	await boids()
+	boids()
 	# check_collision("blocks")
 	vel = vel.normalized() * speed
 	move()
